051f3d4a01defc2321ef0e1356a69f55be71f270
andrercsampaio <aandre308@gmail.com>
2026-03-10 15:46:31 -0300

    Organização: Folder Templates centralizados e Sidebar adicionada no index.html

--- setup/settings.py
+++ setup/settings.py
@@ -58,10 +58,12 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # ADICIONE O CAMINHO DA NOVA PASTA AQUI:
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
